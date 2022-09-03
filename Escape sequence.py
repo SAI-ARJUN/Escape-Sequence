076f66d8e6a3f1322e6e54eb967708aa445c8ed8
SAI ARJUN <112474122+SAI-ARJUN@users.noreply.github.com>
2022-09-03 20:54:04 +0530

    Add files via upload

---
+++ Escape sequence.py
@@ -0,0 +1,21 @@
+String1='''I'm "KGiSL" student'''
+print("Initital String with use of Triple Quotes:")
+print(String1)
+#Escaping single quote
+String1='I\'m a "Engg. Student"'
+print("\n Escaping Single Quote:")
+print(String1)
+#Escaping double quotes
+String1 = "I'm a \IT Student\""
+print("\n Escaping Double Quotes:")
+print(String1)
+#Printing paths with the use of escape sequences
+String1="C:\\Python\\KG\\"
+print("\nEscaping Backslashes:")
+print(String1)
+#Printing paths with the use of New line
+String1="Hi\tKG"
+print("\n Tab:")
+print(String1)
+
+
